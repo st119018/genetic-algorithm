cf12a8c445e0e3ab7868b7b0c5ae64a4b33e3982
elina <elina@Ubuntu.myguest.virtualbox.org>
2025-05-20 11:57:21 +0300

    Add finding best labels

--- experiment.py
+++ experiment.py
@@ -4,10 +4,9 @@ from sklearn.metrics import adjusted_rand_score
 import numpy as np
 import pandas as pd
 from numpy.typing import NDArray
-from typing import Optional
+from typing import Optional, Tuple
 
-
-def score_function(X: NDArray, labels: NDArray):
+def score_function(X: NDArray, labels: NDArray) -> float:
     """
     Compute score function on the input.
 
@@ -38,12 +37,11 @@ def score_function(X: NDArray, labels: NDArray):
     bcd = np.mean(np.linalg.norm(centroids - global_centroid, axis=1))
 
     diff = bcd - wcd
-    sf = 1.0 / (1.0 + np.exp(-diff))
+    sf = float(1.0 / (1.0 + np.exp(-diff)))
     return sf
 
 
-
-def experiment(clusters: int, iterations: int, X: NDArray, y: Optional[NDArray]=None) -> pd.DataFrame:
+def experiment(clusters: int, iterations: int, X: NDArray, y: Optional[NDArray]=None) -> Tuple[pd.DataFrame, NDArray]:
     """
     Compare GA and KMeans clustering using adjusted
     Rand index (ARI) if y is given or score function (SF) if y is None.
@@ -60,33 +58,52 @@ def experiment(clusters: int, iterations: int, X: NDArray, y: Optional[NDArray]=
         Labels
     Returns
     -------
-    results : pd.DataFrame
-        Scores for both methods in each iteration
+    results : Tuple[pd.DataFrame, NDArray]
+        Scores for both methods in each iteration and best found labels
     """
     results = []
-    for i in range(iterations):
-        ga = GAClustering(clusters, random_state=i, max_gen=100, pop_size=100)
-        km = KMeans(n_clusters=clusters, random_state=i, n_init=1)
+    best_labels: Optional[NDArray] = None
+    best_score: Optional[float] = None
 
-        ga.fit(X)
-        labels_ga = ga.predict(X)
+    for i in range(iterations):
+        km = KMeans(n_clusters=clusters, random_state=i, n_init=1)
         labels_km = km.fit_predict(X)
         
         if y is not None:
+            ga = GAClustering(clusters, random_state=i, max_gen=100, pop_size=100)
+            ga.fit(X)
+            labels_ga = ga.predict(X)
+
             ari_score_ga = adjusted_rand_score(y, labels_ga)
             ari_score_km = adjusted_rand_score(y, labels_km)
+
+            if best_score is None:
+                best_score = ari_score_ga
+            if ari_score_ga >= best_score:
+                best_labels = labels_ga
 
             results.append({
                 "ARI (GA)": ari_score_ga,
                 "ARI (KMeans)": ari_score_km
             })
         else:
+            ga = GAClustering(clusters, random_state=i, max_gen=150, pop_size=100, cross_rate=0.9, mut_rate=0.005)
+            ga.fit(X)
+            labels_ga = ga.predict(X)
+
             sf_score_ga = score_function(X, labels_ga)
             sf_score_km = score_function(X, labels_km)
+
+            if best_score is None:
+                best_score = sf_score_ga
+            if sf_score_ga >= best_score:
+                best_labels = labels_ga
 
             results.append({
                 "SF (GA)": sf_score_ga,
                 "SF (KMeans)": sf_score_km
             })
+    if best_labels is None:
+        raise Exception("No valid solution was found.")
 
-    return pd.DataFrame(results)
+    return pd.DataFrame(results), best_labels
